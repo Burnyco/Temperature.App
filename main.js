1bac5fb01aec30960e180a5bf9cfb4bb72d7dca4
Irakli Maisuradze <82453236+Burnyco@users.noreply.github.com>
2021-04-20 09:37:17 +0400

    js1

---
+++ main.js
@@ -0,0 +1,3 @@
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
