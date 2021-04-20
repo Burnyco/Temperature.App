b66583455157ed7977ca33e40ab7dc3caebfeee4
Irakli Maisuradze <82453236+Burnyco@users.noreply.github.com>
2021-04-20 09:40:37 +0400

    main

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const celciusInput = document.querySelector('#celcius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 const kelvinInput = document.querySelector('#kelvin > input');
+
+function roundNum(num) {
+    return Math.round(num * 100) / 100;
+}
+
+function main() {
+    celciusInput.addEventListener('input', cToFToK);
+    fahrenheitInput.addEventListener('input', FtoCandK);
+    kelvinInput.addEventListener('input', kTofToc);
+}
+
+main();
