d5f6b384bf92a3b424b4f5cc3bb0126884eb9e54
Irakli Maisuradze <82453236+Burnyco@users.noreply.github.com>
2021-04-20 09:49:51 +0400

    end

--- main.js
+++ main.js
@@ -6,12 +6,25 @@ function roundNum(num) {
     return Math.round(num * 100) / 100;
 }
 
+function cToFToK() {
+    const cTemp = parseFloat(celciusInput.value);
+    const fTemp = (cTemp * (9 / 5)) + 32;
+    const kTemp = cTemp + 273.15;
+
+    fahrenheitInput.value = roundNum(fTemp);
+    kelvinInput.value = roundNum(kTemp);
+}
+
+
+
+
 
 function FtoCandK() {
 
     const fTemp = parseFloat(fahrenheitInput.value);
     const cTemp = (fTemp - 32) * (5 / 9);
     const kTemp = (fTemp + 459.67) * 5 / 9;
+
     celciusInput.value = roundNum(cTemp);
     kelvinInput.value = roundNum(kTemp);
 
@@ -20,18 +33,16 @@ function FtoCandK() {
 
 
 
+function kTofToc() {
 
+    const kTemp = parseFloat(kelvinInput.value);
+    const cTemp = kTemp - 273.15;
+    const fTemp = 9 / 5 * (kTemp - 273) + 32;
 
+    celciusInput.value = roundNum(cTemp);
+    fahrenheitInput.value = roundNum(fTemp);
 
-
-
-
-
-
-
-
-
-
+}
 
 
 
